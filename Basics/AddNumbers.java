3394db335b61265414b0299be3bf686d80cd5240
Megavarshini R <144305447+Megavarshini1010@users.noreply.github.com>
2025-07-28 18:54:35 +0530

    Add files via upload

---
+++ Basics/AddNumbers.java
@@ -0,0 +1,8 @@
+//Addition of two integer numbers
+public class AddNumbers{
+    public static void main(String[] args) {
+        int a=13;
+        int b=34;
+        System.out.println(a+b);
+    }
+}
